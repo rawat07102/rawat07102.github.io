3f541a4e850747425fd16b1a73e7771f7627d1a0
Ritik Rawat <ritikrawat0710@gmail.com>
2025-01-23 20:17:54 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="flex justify-center items-center w-full h-screen">
+            <h1 className="text-xl">Initial Commit</h1>
+        </div>
+    )
+}
+
+export default App
